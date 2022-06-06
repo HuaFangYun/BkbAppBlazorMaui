407fc53d030a822b6d40dfb1ca5597614bbde84b
Ludwig Pfann <70544031+Paranemi@users.noreply.github.com>
2022-06-06 22:53:39 +0200

    Add project files.

---
+++ BkbAppBlazorMaui/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BkbAppBlazorMaui;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ BkbAppBlazorMaui/Pages/Calendar.razor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BkbAppBlazorMaui.Pages
+{
+    public partial class Calendar
+    {
+        public DateTime Date { get; set; }
+
+        public int Days { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+
+        public DateTime SelectedDate { get; set; }
+
+        protected override void OnInitialized()
+        {
+            SelectedDate = DateTime.Today;
+
+            Days = DateTime.DaysInMonth(SelectedDate.Year, SelectedDate.Month);
+
+            Month = SelectedDate.Month;
+            Year = SelectedDate.Year;
+        }
+
+        protected void PrevMonth()
+        {
+            Month = Month - 1;
+
+            if (Month == 0)
+            {
+                Month = 12;
+                Year = Year - 1;
+            }
+
+            Days = DateTime.DaysInMonth(Year, Month);
+        }
+
+        protected void NextMonth()
+        {
+            Month = Month + 1;
+
+            if (Month == 13)
+            {
+                Month = 1;
+                Year = Year + 1;
+            }
+
+            Days = DateTime.DaysInMonth(Year, Month);
+        }
+    }
+}
